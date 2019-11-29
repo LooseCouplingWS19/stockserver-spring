0d72ccca32fc91422114227107c91b72078649d8
Stefan Basaric <st111465@stud.uni-stuttgart.de>
2019-11-29 08:41:21 +0100

    Init commit

---
+++ src/main/java/de/loosecoupling/assignment3/stockserver/StockServerApplication.java
@@ -0,0 +1,13 @@
+package de.loosecoupling.assignment3.stockserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/loosecoupling/assignment3/stockserver/StockServerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.loosecoupling.assignment3.stockserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
